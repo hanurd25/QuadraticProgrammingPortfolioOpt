bf5cc9e45b33b430c807dd7b6bc88c9f4513df4e
HKU <118258492+hanurd25@users.noreply.github.com>
2026-02-19 17:51:29 +0100

    Update portfolioOpt.R

--- portfolioOpt.R
+++ portfolioOpt.R
@@ -3,18 +3,52 @@ library(quadprog)
 
 
 
-solve.QR(Dmat, dvec, Amat, bvec, meq)
+er <- c(0.10, 0.20, 0.15, 0.25, 0.18)  # Expected returns 
+omega <- matrix(c(
+  0.005, -0.010, 0.004, -0.002,  0.003,
+ -0.010,  0.040, -0.005,  0.000, -0.006,
+  0.004, -0.005,  0.015,  0.010,  0.004,
+ -0.002,  0.000,  0.010,  0.020, -0.001,
+  0.003, -0.006,  0.004, -0.001,  0.012
+), nrow=5, byrow=TRUE) # covariance matrix
 
-# Dmat is covarnace matrix
+nAssets <- length(er)
 
 
 
 
-# quadprog expects 0.5 * x' * D * x, so we pass 2 * Sigma
-result <- solve.QP(
-  Dmat = 2 * Dmat,
-  dvec = dvec,
-  Amat = Amat,
-  bvec = bvec,
-  meq = 1 
+
+
+lambda <- 3
+# adding 2 because quadprog expects 0.5 factor
+Dmat <- 2 * lambda * Omega
+dvec <- -er
+
+
+Amat <- cbind(
+  rep(1, nAssets),      # sum of assets = 1 (100%)
+  diag(nAssets),        # w >= 0  %can't be under 0
+ -diag(nAssets)        # w <= s
 )
+%setting constraints
+%assuming here that every asset should have the same constraints
+bvec <- c(1, rep(0, n_assets), rep(0.2, n_assets))
+meq <- 1  
+
+
+result <- solve.QP(Dmat, dvec, Amat, bvec, meq=meq)
+
+
+weights <- result$solution
+#annualized volatility:
+print(round(weights * 100, 1))  
+
+%calculating risk and printing and annual return
+portfolReturn <- sum(weights * er) * 100
+portfolRisk <- sqrt(weights %*% Omega %*% weights) * 100 * sqrt(252)  
+print(paste("Portfolio Return:", round(portfolReturn, 2), "%"))
+print(paste("Portfolio Risk (Ann Vol):", round(portfolRisk, 2), "%"))
+
+
+
+
