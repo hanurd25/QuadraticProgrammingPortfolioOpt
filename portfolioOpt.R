421628f4803fbf1816b096be9ceec5b8182a0659
HKU <118258492+hanurd25@users.noreply.github.com>
2026-02-19 18:16:34 +0100

    Update portfolioOpt.R
    Did some changes

--- portfolioOpt.R
+++ portfolioOpt.R
@@ -2,7 +2,7 @@ install.packages("quadprog")
 library(quadprog)
 
 
-
+# in this scenario we are working with 5 stocks.
 er <- c(0.10, 0.20, 0.15, 0.25, 0.18)  # Expected returns 
 omega <- matrix(c(
   0.005, -0.010, 0.004, -0.002,  0.003,
@@ -19,10 +19,9 @@ nAssets <- length(er)
 
 
 
-
 lambda <- 3
 # adding 2 because quadprog expects 0.5 factor
-Dmat <- 2 * lambda * Omega
+Dmat <- 2 * lambda * omega
 dvec <- -er
 
 
@@ -31,9 +30,10 @@ Amat <- cbind(
   diag(nAssets),        # w >= 0  %can't be under 0
  -diag(nAssets)        # w <= s
 )
-%setting constraints
-%assuming here that every asset should have the same constraints
-bvec <- c(1, rep(0, n_assets), rep(0.2, n_assets))
+#setting constraints
+#assuming here that every asset should have the same constraints
+#Reduce the first number below 1 when we don't want to be fulli invested
+bvec <- c(1, rep(0, nAssets), rep(0.2, nAssets))
 meq <- 1  
 
 
@@ -41,15 +41,15 @@ result <- solve.QP(Dmat, dvec, Amat, bvec, meq=meq)
 
 
 weights <- result$solution
-#annualized volatility:
+#Weights
 print(round(weights * 100, 1))  
 
-%calculating risk and printing and annual return
-portfolReturn <- sum(weights * er) * 100
-portfolRisk <- sqrt(weights %*% Omega %*% weights) * 100 * sqrt(252)  
-print(paste("Portfolio Return:", round(portfolReturn, 2), "%"))
-print(paste("Portfolio Risk (Ann Vol):", round(portfolRisk, 2), "%"))
 
+portfolioReturn <- sum(weights * er) * 100
+portfolioRisk <- sqrt(weights %*% omega %*% weights) * 100 * sqrt(252)  
+
+print(paste("Expected portfolio return:", round(portfolioReturn, 2), "%"))
+print(paste("Portfolio Risk (Ann Vol):", round(portfolioRisk, 2), "%"))
 
 
 
